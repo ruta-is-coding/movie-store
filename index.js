f7c0a5af63bd02f6899fcd61a89dcea5f63d5863
Rūta J <rutaj@Rutos-draugas.lan>
2024-05-20 22:17:44 +0300

    Created project basis

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+let movies = [];
+
+// Create a new movie
+app.post('/movies', (req, res) => {
+    const { title, productionDate, producer, rating } = req.body;
+    const movie = { id: movies.length + 1, title, productionDate, producer, rating };
+    movies.push(movie);
+    res.status(201).send(movie);
+});
+
+// Get all movies
+app.get('/movies', (req, res) => {
+    res.status(200).send(movies);
+});
+
+// Get a movie by ID
+app.get('/movies/:id', (req, res) => {
+    const movie = movies.find(m => m.id === parseInt(req.params.id));
+    if (!movie) {
+        return res.status(404).send('Movie not found');
+    }
+    res.status(200).send(movie);
+});
+
+// Update a movie by ID
+app.put('/movies/:id', (req, res) => {
+    const movie = movies.find(m => m.id === parseInt(req.params.id));
+    if (!movie) {
+        return res.status(404).send('Movie not found');
+    }
+    const { title, productionDate, producer, rating } = req.body;
+    movie.title = title;
+    movie.productionDate = productionDate;
+    movie.producer = producer;
+    movie.rating = rating;
+    res.status(200).send(movie);
+});
+
+// Delete a movie by ID
+app.delete('/movies/:id', (req, res) => {
+    const movieIndex = movies.findIndex(m => m.id === parseInt(req.params.id));
+    if (movieIndex === -1) {
+        return res.status(404).send('Movie not found');
+    }
+    const deletedMovie = movies.splice(movieIndex, 1);
+    res.status(200).send(deletedMovie[0]);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
